49c992a857ca9b73590c1064c926ee53db7f716c
Chris Jeong <crowjdh@gmail.com>
2016-12-21 20:44:37 +0900

    Get 'output directory' as argument

--- downloader.py
+++ downloader.py
@@ -8,26 +8,18 @@ from bs4 import BeautifulSoup
 from mimetypes import guess_extension
 import unicodedata
 
-if len(sys.argv) < 5:
+if len(sys.argv) < 6:
     print "Usage:"
-    print "With video ids: python filename email pw v videoId [videoIds]"
-    print "With mylist ids: python filename email pw m mylistId"
+    print "With video ids: python filename email pw outputPath v videoId [videoIds]"
+    print "With mylist ids: python filename email pw outputPath m mylistId"
     sys.exit()
 nicoId = sys.argv[1]
 nicoPw = sys.argv[2]
-mode = sys.argv[3]
-args = sys.argv[4:]
-# videoIds = sys.argv[4:]
+outputPath = sys.argv[3]
+mode = sys.argv[4]
+args = sys.argv[5:]
 
 def downloadVideo(sess, url, title):
-    # if True:
-    #     import time
-    #     start = time.time()*1000.0
-    #     print len(sess.get(url).content)
-    #     end = time.time()*1000.0
-    #     print "Time taken: %s" % str(end - start)
-    #     return
-
     response = sess.get(url, stream=True)
     total_length = int(response.headers.get('Content-Length', 0))
     
@@ -41,11 +33,10 @@ def downloadVideo(sess, url, title):
             extension = ""
 
     fileName = title + extension
-    directoryName = "output"
-    filePath = os.path.join(directoryName, fileName)
+    filePath = os.path.join(outputPath, fileName)
 
-    if not os.path.exists(directoryName):
-        os.makedirs(directoryName)
+    if not os.path.exists(outputPath):
+        os.makedirs(outputPath)
 
     with open(filePath, "wb") as handle:
         pbar = tqdm(total = total_length)
@@ -91,6 +82,4 @@ with requests.session() as sess:
             videoUrl = urllib.unquote(apiResultDict['url']).decode('utf8')
             
             downloadVideo(sess, videoUrl, title)
-            
-            # print sess.head(videoUrl).headers
             
